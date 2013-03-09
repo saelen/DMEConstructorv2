8cff56af69e53deff50f559f24a191dbf78b1987
saelen <mterhaar@gmail.com>
2013-03-09 16:01:07 -0500

    Titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def generate_document_title(page)
+      title = 'DME Constructor'
+      if !page.empty?
+          title += ' | ' + page
+      end
+      title
+  end
 end
